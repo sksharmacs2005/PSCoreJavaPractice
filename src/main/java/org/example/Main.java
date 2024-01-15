73e02c34b3d977b30f81ef59123595bdcf413840
Sujeet Sharma <sujsharm3@publicisgroupe.net>
2024-01-15 22:30:23 +0530

    some feature add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,35 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Sujeet");
+
+        String s1="Java";
+        String s2=new String("Java");
+        String s3="Java";
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1==s3);
+
+        int x=10;
+        double y=3.2;
+        char z='A';
+        String str="Hello,World!";
+        System.out.println("x = " + x +  " = y= " + y + " = z = " + z);
+
+        for(int i=0;i<10;i=i++)
+        {
+            i+=1;
+            System.out.println(i+ "Java");
+        }
+        int b=2;
+        int result=Add(b);
+        System.out.println(result);
     }
-}git add 
+
+
+    public static int Add(int x)
+    {
+        int y=4;
+        return x+y;
+
+    }
+}
